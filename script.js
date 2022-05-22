1c60e2a995911606f2d0ae04a7238ecf2cd0926b
enes ay <enesay240@gmail.com>
2022-05-22 19:20:45 +0300

    tasarımda iyileştirmeler yapıldı

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-"use strict";
+(function(){
+     "use strict";
+   
+     var search_btn=document.getElementById('search');
+     var search_form=document.querySelector(".search_form");
+    function function_show()
+    {
+      window.alert("yakında eklenecek");
+    }  
+    search_btn.addEventListener("click",function_search);
+
+     function function_search()
+    {
+      search_form.classList.toggle("active");  
+    }
+    
+   
+   
+     // other functions you may define
+   })();
 
 
-function function1(){
-     alert("yakındaa")
-}
