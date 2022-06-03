733f326eb207616543ebb4fecc361ec3aa32e899
enes ay <enesay240@gmail.com>
2022-06-03 15:51:30 +0300

    hava durumu eklendi, içerik düzenlendi

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">    
     <title>Enes Ay Kişisel Websitesi</title>
@@ -22,71 +22,105 @@
                 <li><a href="#tohakkımda">HAKKIMDA</a> </li>
                 <li><a href="#toözgeçmiş">ÖZGEÇMİŞ</a> </li>
                 <li><a href="#">BLOG</a> </li>
-                <li><a href="#">İLETİŞİM</a> </li>
+                <li><a href="#toiletişim">İLETİŞİM</a> </li>
            </ul>
-           <form class="search_form" method =>
+           <form class="search_form">
 
             <input id="search_input" type="text" onkeyup="function_show()" placeholder="Yazınız">
             <button id="search_submit"><i class="fa-solid fa-magnifying-glass"></i></button></input>
         </form>
        </div> <!-- header finish-->
+
        <div class="buttons">
             <button id="search"><i class="fa-solid fa-magnifying-glass"></i></button>
             <button id="login"><i class="fa-solid fa-user-large"></i></button>
+        </div>
+        <div class="havaDurumu">
+           <h2 id="city_weather">
+               Bandırma
+            </h2>
+            <p id="weather_result"  >
+            
+            </p>
+            <p id="details"  >
+               
+            </p>
+        </div>
+        <!-- CONTENTS START -->
 
-       </div>
        <section id="tohome" class="home">
            <div class="content">
                <h3>
                   ENES AY KİŞİSEL WEBSİTESİ
                </h3>
-               <p>
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                   Consequatur magnam harum saepe veritatis obcaecati soluta, 
-                   dignissimos iste ducimus mollitia accusamus, 
-                   id, asperiores inventore debitis? Nisi eaque quam labore at praesentium.
-               </p>
+            
            </div>
-       </section>
+         
+        </section>
+    
        <section id="tohakkımda" class="hakkımda">
 
                     <h2>
                         HAKKIMDA
                     </h2>
-                    <p >
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-                        Suscipit amet neque quidem aliquam sapiente error veniam, 
-                        totam ullam illum recusandae fuga debitis 
-                        commodi fugiat facere iusto voluptate placeat quas ducimus?
+                    <p>
+                    2002 Eskişehir doğumluyum.Bandırma Onyedi Eylül Üniversitesi'nde
+                     Bilgisayar Mühendisliği 2.sınıf öğrencisiyim.
         
                     </p>
 
        </section>
+
        <section id="toözgeçmiş" class="özgeçmiş">
            <h2>
                ÖZGEÇMİŞ
            </h2>
+           <p id="name">ENES AY</p>
            <p>
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-                debitis deserunt blanditiis veritatis temporibus, porro ullam perferendis fugiat repudiandae! 
-               Necessitatibus ipsum vel temporibus nam? Earum, mollitia praesentium. Expedita, quae itaque.
+               
+               <br>
+               <b>Kişisel Bilgiler</b><br>
+               2002, Eskişehir<br>
+               enesay240@gmail.com<br>
+               <b>Eğitim Bilgileri</b><br>
+               Bandırma Onyedi Eylül Üniversitesi<br>
+               Bilgisayar Mühendisliği 2020-2024<br>
+               <b>İlgi Alanları</b><br>
+               IoT, Android Programlama, Siber Güvenlik <br>
+               <b>Dijital Beceriler</b><br>
+               Java, Kotlin, C/C++
+
            </p>
        </section>
 
+       <section id="toiletişim" class="iletişim">
+               <h2>
+                 İLETİŞİM
 
+                </h2>
+                <p>
+                    enesay240@gmail.com<br>
+                    Odunpazarı/Eskişehir
+                </p>
+        </section>
+      <!-- CONTENTS FINISH -->
 
     <div class="social">
-        <a id="github" href="https://github.com/enes-ay"><i class="fa fa-github"></i></a>
-        <a id="gmail"href="https://www.google.com/intl/tr/gmail/about/" target="_blank" class="google"><i class="fa fa-google"></i></a>
-        <a id="linkedin"href="https://www.linkedin.com/in/enes-ay-2446191b3/" target="_blank"  class="linkedin"><i class="fa fa-linkedin"></i></a>
-        <a id="instagram"href="https://www.instagram.com/ay__enes/?hl=tr" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a>
+
+        <a id="github" href="https://github.com/enes-ay" target="_blank"><i class="fa fa-github"></i></a>
+        <a id="gmail" href="https://www.google.com/intl/tr/gmail/about/" target="_blank" ><i class="fa fa-google"></i></a>
+        <a id="linkedin" href="https://www.linkedin.com/in/enes-ay-2446191b3/" target="_blank" ><i class="fa fa-linkedin"></i></a>
+        <a id="instagram" href="https://www.instagram.com/ay__enes/?hl=tr" target="_blank"><i class="fa fa-instagram"></i></a>
 
 
     </div>
+
 
     <div class="footer">
         <p id="footer_info">All rights reserved <b>|</b> 2022</p>
     </div>
+
+    
     <script src="script.js" ></script> 
 </body>
 </html>
@@ -95,6 +129,4 @@
 <?php
 
 include ("connect.php");
-
-
 ?>
--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 (function(){
      "use strict";
-     const URL = 'https://api.openweathermap.org/data/2.5/weather?q=tokyo&appid=72bb3ad09090ff550b65d6bd8660aacb';
+     const URL = 'https://api.openweathermap.org/data/2.5/weather?q=bandırma&appid=72bb3ad09090ff550b65d6bd8660aacb';
    
      var search_btn=document.getElementById('search');
      var search_form=document.querySelector(".search_form");
      var login=document.getElementById("login")
      var search_input=document.getElementById("search_input");
+
+     login.addEventListener("click",function_login);
+
+     function function_login(){
+      alert("eklenecek");
+     }
     
     /*Search Button start */
     function function_show()
@@ -26,7 +32,18 @@
     /*Search Button finish */
 
      /*Weather api start*/
-     window.onload=makeRequest();
+     
+     var  displayweather=response=>{
+      let sonuc=document.getElementById("weather_result");
+      let details=document.getElementById("details");
+      sonuc.innerText=`${Math.round(response.main.temp)-274}°C`
+      details.innerHTML=`Nem oranı: %${response.main.humidity}\n
+      min:${Math.round(response.main.temp_min-274)}°C/max:${Math.round(response.main.temp_max-274)}°C`
+
+      
+      
+    } 
+    window.onload=makeRequest();
         
     function makeRequest() {  
        fetch(URL)
@@ -34,18 +51,9 @@
       .then(displayweather)
     }
 
-    const  displayweather=response=>{
-      let sonuc=document.getElementById("weather_result");
-      sonuc.innerText=` Sıcaklık:${Math.round(response.main.temp)-274}°C\nNem oranı:${response.main.humidity}\n
-      min sıcaklık:${Math.round(response.main.temp_min-274)}\nmax sıcaklık:${Math.round(response.main.temp_max-274)}`
 
-      let h2=document.getElementById("city_weather");
-     
-
-    }
     
-    /* Weather api finish */
-       
+      /* Weather api finish */
    
    })();
 
